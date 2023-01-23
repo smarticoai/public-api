e77b28a2e6ec89d080b8bd330414e91c423bba75
Anton Antropov <anton@smartico.ai>
2023-01-23 14:59:31 +0200

    fix avatar

--- src/Core/CoreUtils.ts
+++ src/Core/CoreUtils.ts
@@ -3,7 +3,11 @@ class CoreUtils {
 
     public static avatarUrl = (avatar_id: string, avatarDomain: string): string => {
         if (avatar_id !== null && avatar_id !== undefined && !(avatar_id.startsWith && avatar_id.startsWith('http'))) {
-            return avatarDomain + '/avatar/' + avatar_id
+            if (avatarDomain.endsWith('/')) {
+                avatarDomain = avatarDomain.slice(0, -1) + '/avatar/' + avatar_id
+            } else {
+                avatarDomain = avatarDomain + '/avatar/' + avatar_id
+            }
         } else {
             return avatar_id
         }
