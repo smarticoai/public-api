075612d31ff531a194bc33ebabb4968fc1151f10
Oleh Yahont <olegyahont15@gmail.com>
2023-01-23 11:51:05 +0200

    fix Core index.ts exports

--- src/Core/index.ts
+++ src/Core/index.ts
@@ -1,22 +1,9 @@
-import { ActivityTypeLimited } from './ActivityTypeLimited'
-import { CoreUtils } from './CoreUtils'
-import { GetLabelInfoRequest } from './GetLabelInfoRequest'
-import { GetLabelInfoResponse } from './GetLabelInfoResponse'
-import { GetTranslationsRequest } from './GetTranslationsRequest'
-import { GetTranslationsResponse } from './GetTranslationsResponse'
-import { PublicLabelSettings } from './PublicLabelSettings'
-import { ResponseIdentify } from './ResponseIdentify'
-import { TranslationArea } from './TranslationArea'
-
-
-export {
-    GetTranslationsRequest,
-    GetTranslationsResponse,
-    GetLabelInfoRequest,
-    GetLabelInfoResponse,
-    TranslationArea,
-    PublicLabelSettings,
-    ResponseIdentify,
-    ActivityTypeLimited,
-    CoreUtils
-}
+export * from './ActivityTypeLimited'
+export * from './CoreUtils'
+export * from './GetLabelInfoRequest'
+export * from './GetLabelInfoResponse'
+export * from './GetTranslationsRequest'
+export * from './GetTranslationsResponse'
+export * from './PublicLabelSettings'
+export * from './ResponseIdentify'
+export * from './TranslationArea'
