e82cbc8ccac5ae08fd718bb4dc786bf60fdecf53
Oleh Yahont <olegyahont15@gmail.com>
2023-01-24 14:37:40 +0200

    Update MarkInboxMessageReadRequest.ts and MarkInboxMessageStarredRequest.ts

--- src/Inbox/MarkInboxMessageReadRequest.ts
+++ src/Inbox/MarkInboxMessageReadRequest.ts
@@ -3,5 +3,5 @@ import { ProtocolMessage } from "../Base/ProtocolMessage";
 
 export interface MarkInboxMessageReadRequest extends ProtocolMessage {
     engagement_uid?: string;
-    mark_all?: boolean;
+    all_read?: boolean;
 }
--- src/Inbox/MarkInboxMessageStarredRequest.ts
+++ src/Inbox/MarkInboxMessageStarredRequest.ts
@@ -3,4 +3,5 @@ import { ProtocolMessage } from "../Base/ProtocolMessage";
 
 export interface MarkInboxMessageStarredRequest extends ProtocolMessage {
     engagement_uid: string;
+    is_starred?: boolean;
 } 
