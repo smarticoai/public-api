db766db5dae6d96e7b585b6595f61152b50120c1
Oleh Yahont <olegyahont15@gmail.com>
2023-04-04 11:42:26 +0300

    add giftbox game type

--- src/MiniGames/SAWGameType.ts
+++ src/MiniGames/SAWGameType.ts
@@ -1,11 +1,13 @@
 export enum SAWGameType {
     SpinAWheel = 1,
     ScratchCard = 2,
-    MatchX = 3
+    MatchX = 3,
+    GiftBox = 4
 }
 
 export const SAWGameTypeName = {
     [SAWGameType.SpinAWheel]: "wheel",
     [SAWGameType.ScratchCard]: "scratch",
     [SAWGameType.MatchX]: "matchx",
+    [SAWGameType.GiftBox]: "giftbox",
 }
