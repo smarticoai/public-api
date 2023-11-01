366bf16d3225407ea0edf64f3d1656ddadc678e3
Oleh Yahont <oleh.y@smartico.ai>
2023-11-01 11:54:48 +0200

    extent ActivityTypeLimited

--- src/Core/ActivityTypeLimited.ts
+++ src/Core/ActivityTypeLimited.ts
@@ -2,7 +2,18 @@ export enum ActivityTypeLimited {
 
     DoNothing = 8,
     Points = 12,
+    GiveMission = 14,
+    StartAnotherCampaign = 15,
+    MiniGameAttempt = 16,
+    DeductPoints = 21,
+    ResetPoints = 22,
     Popup = 30,
     Inbox = 31,
+    Slack = 70,
+    Email_To_Customer_Support = 80,
+    Email_To_Risk_Operator = 90,
     Bonus = 100,
+    WebHook = 200,
+    TagCustomer = 504,
+    UnTagCustomer = 505,
 }
