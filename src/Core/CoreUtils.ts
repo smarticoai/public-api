f381ed27e3ab50ec23c7394c5dca48fca28df513
Anton Antropov <anton@smartico.ai>
2023-12-13 17:51:14 +0200

    sentry crash

--- src/Core/CoreUtils.ts
+++ src/Core/CoreUtils.ts
@@ -2,7 +2,7 @@
 class CoreUtils {
 
     public static avatarUrl = (avatar_id: string, avatarDomain: string): string => {
-        if (avatar_id !== null && avatar_id !== undefined && !(avatar_id.startsWith && avatar_id.startsWith('http'))) {
+        if (avatarDomain && avatar_id !== null && avatar_id !== undefined && !(avatar_id.startsWith && avatar_id.startsWith('http'))) {
             if (avatarDomain.endsWith('/')) {
                 return avatarDomain.slice(0, -1) + '/avatar/' + avatar_id
             } else {
