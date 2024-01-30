768e6217eb636de06fb3a91fcd425191af75ba59
Anton Antropov <anton@smartico.ai>
2024-01-30 09:09:45 +0200

    remove points period 1 and 2

--- src/Core/PublicProperties.ts
+++ src/Core/PublicProperties.ts
@@ -2,8 +2,6 @@ export interface PublicProperties {
     core_user_language?: string;
     ach_points_balance?: number;
     ach_points_ever?: number;
-    ach_points_board_period_type_1?: number;
-    ach_points_board_period_type_2?: number;
     ach_level_current_id?: number;
     ach_level_current?: string;
     core_is_test_account?: boolean;
