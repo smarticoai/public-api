0677c48e847774321edb436b74d76bfb6d83ea9b
Anton Antropov <anton@smartico.ai>
2024-04-18 00:20:28 +0300

    SAW: adding jackpot amount in SAWDoSpinResponse

--- src/MiniGames/SAWDoSpinResponse.ts
+++ src/MiniGames/SAWDoSpinResponse.ts
@@ -7,6 +7,7 @@ export interface SAWDoSpinResponse extends ProtocolResponse {
     errMsg?: string;
     request_id: string; // guid
     saw_prize_id: number;
+    jackpot_amount?: number; // jackpot amount in case user won jackpot type of prize
     first_spin_in_period: number;
     visitor_win_uuid?: string;
 };
