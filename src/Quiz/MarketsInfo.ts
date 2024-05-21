248866fc2970a97f8302097107d01d9b21803312
Oleh Yahont <oleh.y@smartico.ai>
2024-05-21 18:32:48 +0300

    Quiz: WIP

--- src/Quiz/MarketsInfo.ts
+++ src/Quiz/MarketsInfo.ts
@@ -1,17 +1,194 @@
+import { MarketsValueType } from "./MarketsAnswersType";
 import { QuizMarketType } from "./MarketsType";
 
-const quizAnswerHomeTeamReplacementText = '%HomeTeam%';
-const quizAnswerAwayTeamReplacementText = '%AwayTeam%';
+const quizAnswerHomeTeamReplacementText = '{quiz_home_team}';
+const quizAnswerAwayTeamReplacementText = '{quiz_away_team}';
+const drawReplacementText = '{quiz_draw}';
+const quizYesReplacementText = '{yes}';
+const quizNoReplacementText = '{no}';
+const quizOddReplacementText = '{odd}';
+const quizEvenReplacementText = '{even}';
+
+const yesNoAnswers = [
+    { text: quizYesReplacementText, value: MarketsValueType.Yes },
+    { text: quizNoReplacementText, value: MarketsValueType.No }
+]
+
+const homeAwayAnswers = [
+    { text: quizAnswerHomeTeamReplacementText, value: MarketsValueType.HomeTeam },
+    { text: quizAnswerAwayTeamReplacementText, value: MarketsValueType.AwayTeam }
+]
+
+const homeAwayDrawAnswers = [
+    ...homeAwayAnswers,
+    { text: drawReplacementText, value: MarketsValueType.Draw },
+]
 
 export const marketsInfo = [
     {
         type: QuizMarketType.OneXTwo,
-        name: '1X2',
-        question: { text: 'Who will win the match ?', trKey: 'quiz_answer_1x2' },
+        name: '1x2',
+        question: { text: 'Who will win the match ?', trKey: 'quizQuestion_1x2'},
+        answers: homeAwayDrawAnswers
+    },
+    {
+        type: QuizMarketType.OneXTwoHalftime,
+        name: '1x2 Halftime',
+        question: { text: 'Who will lead the first half of the match ?', trKey: 'quizQuestion_x2half' },
+        answers: homeAwayDrawAnswers
+    },
+    {
+        type: QuizMarketType.HalftimeFulltime,
+        name: 'Halftime / Fulltime',
+        question: { text: 'Who will lead the first half and who will win the match ?', trKey: 'quizQuestion__HalftimeFulltime' },
         answers: [
-            { text: quizAnswerHomeTeamReplacementText, trKey: 'quiz_answer_1x2_home' },
-            { text: 'Draw', trKey: 'quiz_answer_1x2_draw' },
-            { text: quizAnswerAwayTeamReplacementText, trKey: 'quiz_answer_1x2_away' }
+            { text: `${quizAnswerHomeTeamReplacementText} / ${quizAnswerHomeTeamReplacementText}`, value: MarketsValueType.HomeTeamHomeTeam },
+            { text: `${quizAnswerHomeTeamReplacementText} / ${drawReplacementText}`, value: MarketsValueType.HomeTeamDraw },
+            { text: `${quizAnswerHomeTeamReplacementText} / ${quizAnswerAwayTeamReplacementText}`,  value: MarketsValueType.HomeTeamAwayTeam },
+            { text: `${drawReplacementText} / ${quizAnswerHomeTeamReplacementText}`, value: MarketsValueType.DrawHomeTeam },
+            { text: `${drawReplacementText} / ${drawReplacementText}`,  value: MarketsValueType.DrawDraw },
+            { text: `${drawReplacementText} / ${quizAnswerAwayTeamReplacementText}`, value: MarketsValueType.DrawAwayTeam },
+            { text: `${quizAnswerAwayTeamReplacementText} / ${quizAnswerHomeTeamReplacementText}`, value: MarketsValueType.AwayTeamHomeTeam },
+            { text: `${quizAnswerAwayTeamReplacementText} / ${drawReplacementText}`,  value: MarketsValueType.AwayTeamDraw },
+            { text: `${quizAnswerAwayTeamReplacementText} / ${quizAnswerAwayTeamReplacementText}`, value: MarketsValueType.AwayTeamAwayTeam }
         ]
-    }
+    },
+    {
+        type: QuizMarketType.FirstGoal,
+        name: 'First Goal',
+        question: { text: 'Who will score the first goal ?', trKey: 'quizQuestion_firstGoal' },
+        answers: homeAwayDrawAnswers
+    },
+    {
+        type: QuizMarketType.LastGoal,
+        name: 'Last Goal',
+        question: { text: 'Who will score the last goal ?', trKey: 'quizQuestion_lastGoal' },
+        answers: homeAwayAnswers
+    },
+    {
+        type: QuizMarketType.DoubleChance,
+        name: 'Double Chance',
+        question: { text: 'Who will win the match? Choose a combination: ?', trKey: 'quizQuestion_doubleChance' },
+        answers: [
+            { text: `${drawReplacementText} or ${quizAnswerHomeTeamReplacementText}`, value: MarketsValueType.HomeTeamDraw },
+            { text: `${quizAnswerHomeTeamReplacementText} or ${quizAnswerAwayTeamReplacementText}`, value: MarketsValueType.HomeTeamAwayTeam },
+            { text: `${drawReplacementText} or ${quizAnswerAwayTeamReplacementText}`, value: MarketsValueType.AwayTeamDraw }
+        ]
+    },
+    {
+        type: QuizMarketType.OverUnder2_5,
+        name: 'Over/Under 2.5',
+        question: { text: 'Will there be 3 or more goals in the match?', trKey: 'quizQuestion_overUnder2_5' },
+        answers: yesNoAnswers
+    },
+    {
+        type: QuizMarketType.OverUnder1_5Halftime,
+        name: 'Over/Under 1.5 Halftime',
+        question: { text: 'Will there be 2 or more goals in the first half?', trKey: 'quizQuestion_overUnder1_5Halftime' },
+        answers: yesNoAnswers
+    },
+    {
+        type: QuizMarketType.OverUnder1_5__2ndHalf,
+        name: 'Over/Under 1.5 2nd Half',
+        question: { text: 'Will there be 2 or more goals in the second half?', trKey: 'quizQuestion_overUnder1_5__2ndHalf' },
+        answers: yesNoAnswers
+    },
+    {
+        type: QuizMarketType.RedCard,
+        name: 'Red Card',
+        question: { text: 'Will there be a red card in the match?', trKey: 'quizQuestion_redCard' },
+        answers: yesNoAnswers
+    },
+    {
+        type: QuizMarketType.TotalCorners8_5,
+        name: 'Total Corners 8.5',
+        question: { text: 'Will there be 9 or more corners in the match?', trKey: 'quizQuestion_totalCorners8_5' },
+        answers: yesNoAnswers
+    },
+    {
+        type: QuizMarketType.TotalCorners9_5,
+        name: 'Total Corners 9.5',
+        question: { text: 'Will there be 10 or more corners in the match?', trKey: 'quizQuestion_totalCorners9_5' },
+        answers: yesNoAnswers
+    },
+    {
+        type: QuizMarketType.Corners1x2,
+        name: 'Corners 1x2',
+        question: { text: 'Which team will have more corners in the match?', trKey: 'quizQuestion_corners1x2' },
+        answers: homeAwayAnswers
+    },
+    {
+        type: QuizMarketType.One_Two,
+        name: '1-2',
+        question: { text: 'Who will win the match ?', trKey: 'quizQuestion_1-2'},
+        answers: homeAwayAnswers
+    },
+    {
+        type: QuizMarketType.Overtime,
+        name: 'Overtime',
+        question: { text: 'Will there be overtime in the match?', trKey: 'quizQuestion_overtime' },
+        answers: yesNoAnswers
+    },
+    {
+        type: QuizMarketType.OddEven,
+        name: 'Odd/Even',
+        question: { text: 'Is the sum of all points scored in the game going to be an odd number or even?', trKey: 'quizQuestion_oddEven' },
+        answers: [
+            { text: quizOddReplacementText, value: MarketsValueType.Odd },
+            { text: quizEvenReplacementText, value: MarketsValueType.Even }
+        ]
+    },
+    {
+        type: QuizMarketType.OddEvenHomeAway,
+        name: 'Odd/Even Home/Away',
+        question: { text: 'Which team will win and what will be the sum of the points (Odd or Even)?', trKey: 'quizQuestion_oddEvenHomeAway' },
+        answers: [
+            { text: `${quizAnswerHomeTeamReplacementText} - ${quizOddReplacementText}`, value: MarketsValueType.HomeOdd },
+            { text: `${quizAnswerHomeTeamReplacementText} - ${quizEvenReplacementText}`, value: MarketsValueType.HomeEven },
+            { text: `${quizAnswerAwayTeamReplacementText} - ${quizOddReplacementText}`, value: MarketsValueType.AwayOdd },
+            { text: `${quizAnswerAwayTeamReplacementText} - ${quizEvenReplacementText}`, value: MarketsValueType.AwayEven }
+        ]
+    },
+    {
+        type: QuizMarketType.FirstSet,
+        name: 'First Set',
+        question: { text: 'Who will win the first set ?', trKey: 'quizQuestion_firstSet' },
+        answers: homeAwayAnswers
+    },
+    {
+        type: QuizMarketType.SecondSet,
+        name: 'Second Set',
+        question: { text: 'Who will win the second set ?', trKey: 'quizQuestion_secondSet' },
+        answers: homeAwayAnswers
+    },
+    {
+        type: QuizMarketType.ThirdSet,
+        name: 'Third Set',
+        question: { text: 'Who will win the third set ?', trKey: 'quizQuestion_thirdSet' },
+        answers: homeAwayAnswers
+    },
+    {
+        type: QuizMarketType.FourthSet,
+        name: 'Fourth Set',
+        question: { text: 'Who will win the fourth set ?', trKey: 'quizQuestion_fourthSet' },
+        answers: homeAwayAnswers
+    },
+    {
+        type: QuizMarketType.FifthSet,
+        name: 'Fifth Set',
+        question: { text: 'Who will win the fifth set ?', trKey: 'quizQuestion_fifthSet' },
+        answers: homeAwayAnswers
+    },
+    {
+        type: QuizMarketType.PlayerOneWinsOneSet,
+        name: 'Player One Wins One Set',
+        question: { text: 'Will Player One win at least one set?', trKey: 'quizQuestion_playerOneWinsOneSet' },
+        answers: yesNoAnswers
+    },
+    {
+        type: QuizMarketType.PlayerTwoWinsOneSet,
+        name: 'Player Two Wins One Set',
+        question: { text: 'Will Player Two win at least one set?', trKey: 'quizQuestion_playerTwoWinsOneSet' },
+        answers: yesNoAnswers
+    },
 ]
--- src/Quiz/index.ts
+++ src/Quiz/index.ts
@@ -3,3 +3,4 @@ export * from './MarketsType';
 export * from './ScoreResultTypes';
 export * from './MarketsInfo';
 export * from './MarketsPerSport';
+export * from './MarketsAnswersType';
