e506bff7b7da09119f482b8c786b60a5882b8575
Ihor Izviekov <ihor.i@smartico.ai>
2024-07-10 09:51:56 +0300

    SAWGetTemplatesResponse update

--- src/MiniGames/SAWGetTemplatesResponse.ts
+++ src/MiniGames/SAWGetTemplatesResponse.ts
@@ -41,6 +41,7 @@ export const SAWTemplatesTransform = (items: SAWTemplate[]): TMiniGameTemplate[]
             promo_image: r.saw_template_ui_definition.promo_image,
             promo_text: r.saw_template_ui_definition.promo_text,
             custom_data: IntUtils.JsonOrText(r.saw_template_ui_definition.custom_data),
+            expose_game_stat_on_api: r.expose_game_stat_on_api,
 
             next_available_spin_ts: r.next_available_spin_ts,
 
@@ -57,6 +58,8 @@ export const SAWTemplatesTransform = (items: SAWTemplate[]): TMiniGameTemplate[]
                     aknowledge_message: p.saw_prize_ui_definition.aknowledge_message,
                     acknowledge_dp: p.saw_prize_ui_definition.acknowledge_dp,
                     acknowledge_action_title: p.saw_prize_ui_definition.acknowledge_action_title,
+                    pool: p.pool,
+                    wins_count: p.wins_count,
                 }
                 return y;
             })
