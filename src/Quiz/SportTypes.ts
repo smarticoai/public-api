3acb679937c76938d331f03151b207a0d6dc4493
Oleh Yahont <oleh.y@smartico.ai>
2024-09-05 10:12:54 +0300

    fix quiz sport ids

--- src/Quiz/SportTypes.ts
+++ src/Quiz/SportTypes.ts
@@ -1,7 +1,7 @@
 export enum QuizSportType {
 	Soccer = 1,
+	Basketball = 2,
 	Tennis = 5,
-	Basketball = 6,
 }
 
 export const quizSupportedSports = [QuizSportType.Basketball, QuizSportType.Soccer, QuizSportType.Tennis];
