c15e8c721135797d06e109fbf963e6b577443e97
Oleh Yahont <oleh.y@smartico.ai>
2024-10-11 11:58:35 +0300

    extend GetAchievementsUserInfoResponse

--- src/Core/GetAchievementsUserInfoResponse.ts
+++ src/Core/GetAchievementsUserInfoResponse.ts
@@ -3,4 +3,10 @@ import { ProtocolResponse } from '../Base/ProtocolResponse';
 export interface GetAchievementsUserInfoResponse extends ProtocolResponse {
 	level_counter_1?: number;
 	level_counter_2?: number;
+	points_balance: number;
+    points_ever: number;
+    current_level: number;
+    points_board_period_type_1: number;
+    points_board_period_type_2: number;
+    points_board_period_type_3: number;
 }
