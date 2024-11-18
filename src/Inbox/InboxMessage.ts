ef86d8271f9c354047e04bfa0b1b42675e7b75ce
Anton Antropov <anton@smartico.ai>
2024-11-18 21:58:17 +0200

    doc for inbox proto message

--- src/Inbox/InboxMessage.ts
+++ src/Inbox/InboxMessage.ts
@@ -2,21 +2,39 @@ import { TInboxMessageBody } from '../WSAPI/WSAPITypes';
 import { InboxMessageType } from './InboxMessageType';
 
 export interface InboxMessageBody {
+	// URL or deep-link that neeed to be executed when user clicks on the message
 	action: string;
+	// the body of the message in case it's 'simple' inbox message
 	body: string;
+	// type of message, deprecated
 	type: InboxMessageType;
+	// image of the icon
 	image: string;
+	// title of the message
 	title: string;
+	// HTML body of the message in case it's 'rich' inbox message
 	html_body: string;
+	// set of the buttons with text on the button and action that could be URL or deep-link
 	additional_buttons?: { inbox_cta_text: string; action: string }[];
+	// indicator if the message need to be shown when sent and overlay or it should be shown only in the inbox, when user open 'inbox' interface explicitly
+	// default is 'true'
+	show_preview?: boolean;
+	// duration in seconds for how long to show preview, default is 7 seconds
+	show_duration_sec?: number;
 }
 
 export interface InboxMessage {
+	// date when the message was created
 	createDate: string;
+	// body of the message according to the interface
 	body: InboxMessageBody;
+	// unique identifier of the message that connects it to the campaign from which it was sent
 	engagement_uid: string;
+	// flag that indicates if the message was read by the user
 	is_read: boolean;
+	// flag that indicates if the message was marked as favorite by the user
 	is_starred: boolean;
+	// flag that indicates if the message was deleted by the user. Such messages are not returned in the response and property is used only internaly in Smartico
 	is_deleted?: boolean;
 }
 
