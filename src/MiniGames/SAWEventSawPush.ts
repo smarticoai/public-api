b039da9a04cb57e2ba5afbcc1ad57f322bfe9556
Anton Antropov <anton@smartico.ai>
2024-12-03 21:20:24 +0200

    SMR-19725
    Expand SAWEventSawPush with saw_game_type_id

--- src/MiniGames/SAWEventSawPush.ts
+++ src/MiniGames/SAWEventSawPush.ts
@@ -1,6 +1,8 @@
 import { ProtocolMessage } from "../Base/ProtocolMessage";
+import { SAWGameType } from "./SAWGameType";
 
 export interface SAWEventSawPush extends ProtocolMessage {
 	pending_message_id: number;
 	saw_template_id: number;
+	saw_game_type_id: SAWGameType;
 }
