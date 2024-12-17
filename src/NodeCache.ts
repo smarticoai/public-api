ca8951d7e5764724b19217bea376009a1fe610c6
Anton Antropov <anton@smartico.ai>
2024-12-17 15:39:04 +0200

    fix cache implementation. in fact the cache was shared through all instances because storage was static variable

--- src/NodeCache.ts
+++ src/NodeCache.ts
@@ -1,17 +1,17 @@
 class NodeCache {
-	private static ttlChecker: NodeJS.Timeout;
+	private ttlChecker: NodeJS.Timeout;
 
-	private static cache: { [key: string]: any } = {};
+	private cache: { [key: string]: any } = {};
 
-	constructor() {
-		if (NodeCache.ttlChecker === undefined) {
-			NodeCache.ttlChecker = setInterval(() => {
+	constructor() {		
+		if (this.ttlChecker === undefined) {
+			this.ttlChecker = setInterval(() => {
 				const now = new Date().getTime();
-				for (const key in NodeCache.cache) {
-					if (NodeCache.cache.hasOwnProperty(key)) {
-						const o = NodeCache.cache[key];
+				for (const key in this.cache) {
+					if (this.cache.hasOwnProperty(key)) {
+						const o = this.cache[key];
 						if (o.ttl < now) {
-							delete NodeCache.cache[key];
+							delete this.cache[key];
 						}
 					}
 				}
@@ -20,27 +20,27 @@ class NodeCache {
 	}
 
 	public get(key: string): any {
-		const o = NodeCache.cache[key];
+		const o = this.cache[key];
 		if (o !== undefined && o.ttl > new Date().getTime()) {
 			return o.value;
 		}
 	}
 
 	public set(key: string, value: any, ttlSeconds: number = 60) {
-		NodeCache.cache[key] = {
+		this.cache[key] = {
 			value,
 			ttl: new Date().getTime() + ttlSeconds * 1000,
 		};
 	}
 
 	public remove(key: string) {
-		if (NodeCache.cache.hasOwnProperty(key)) {
-			delete NodeCache.cache[key];
+		if (this.cache.hasOwnProperty(key)) {
+			delete this.cache[key];
 		}
 	}
 
 	public flushAll() {
-		NodeCache.cache = {};
+		this.cache = {};
 	}
 }
 
