47bf9ae98c374138a21e6e4eb659205a2a005722
Taras Bondaruk <therealtoresto@gmail.com>
2025-03-06 14:04:06 +0200

    [FEAT] - RAF: extended raffle draw public meta

--- src/Raffle/RaffleDraw.ts
+++ src/Raffle/RaffleDraw.ts
@@ -8,6 +8,10 @@ interface RaffleDrawPublicMeta {
     description: string;
     /** URL of the image that represents the draw */
     image_url: string;
+    /** URL of the icon that represents the draw */
+    icon_url: string;
+    /** URL of the background image that will be used in the draw list item */
+    background_image_url: string;
 }
 
 enum RaffleDrawInstanceState {
