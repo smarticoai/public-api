f4e366fd9b62310765e61966536d02305253e8b0
Oleh Yahont <oleh.y@smartico.ai>
2025-03-21 16:02:31 +0200

    store items err codes explanations

--- src/Store/BuyStoreItemErrorCode.ts
+++ src/Store/BuyStoreItemErrorCode.ts
@@ -1,11 +1,18 @@
 export enum BuyStoreItemErrorCode {
-	FAILED_TO_BUY_SHOP_ITEM = 121,
+	/** User don't have enough points on balance */
 	SHOP_NO_BALANCE = 11000,
+	/** Wrong shop item id */
 	SHOP_WRONG_LABEL = 11001,
+	/** Failed to buy bonus item, probably something wrong with the bonus configuration. */
 	SHOP_FAILED_TO_BUY_BONUS = 11002, // remote product restriction
-	SHOP_FAILED_TO_BUY_SHOP_ITEM_CONDITION = 11003, // when not visible but trying to buy. Shoudnt happen
+	/** Failed to buy store item because segment conditions are set by operator for specific CRM item */
+	SHOP_FAILED_TO_BUY_SHOP_ITEM_CONDITION = 11003, // when not visible but trying to buy. shouldn't happen
+	/**  Segment conditions are set by operator for specific CRM item and user not matching to this conditions */
 	SHOP_FAILED_TO_BUY_SHOP_ITEM_CONDITION_PURSHASE = 11004, // when visible but not matching to purchase segment
+	/** Failed to buy matching bonus item */
 	SHOP_FAILED_TO_BUY_MATCHING_BONUS = 11005,
+	/** Failed to buy item because of limit of max items is reached */
 	SHOP_FAILED_MAX_BOUGHT_ITEMS_REACHED = 11006,
+	/** Failed to buy item because no more items are available */
 	SHOP_FAILED_POOL_EMPTY = 11009,
 }
