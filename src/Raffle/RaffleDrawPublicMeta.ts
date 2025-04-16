6cf693b3bb3f891ac2f7c1f63162c9941736db13
Leonid <leonid.p@smartico.ai>
2025-04-16 15:56:05 +0300

    drawPublicMeta interface improvement

--- src/Raffle/RaffleDrawPublicMeta.ts
+++ src/Raffle/RaffleDrawPublicMeta.ts
@@ -1,16 +1,18 @@
 export interface RaffleDrawPublicMeta {
-    /** Name of the draw, e.g. 'Daily draw' */
-    name: string;
-    /** Description of the draw */
-    description: string;
-    /** URL of the image that represents the draw */
-    image_url: string;
-    /** URL of the icon that represents the draw */
-    icon_url: string;
-    /** URL of the background image that will be used in the draw list item */
-    background_image_url: string;
-    /** URL of the moible background image that will be used in the draw list item */
-    background_image_url_mobile: string;
-    /** Show if the draw is grand and is marked as special */
-    is_grand: boolean;
+	/** Name of the draw, e.g. 'Daily draw' */
+	name: string;
+	/** Description of the draw */
+	description: string;
+	/** URL of the image that represents the draw */
+	image_url: string;
+	/** URL of the moible image that represents the draw */
+	image_url_mobile: string;
+	/** URL of the icon that represents the draw */
+	icon_url: string;
+	/** URL of the background image that will be used in the draw list item */
+	background_image_url: string;
+	/** URL of the moible background image that will be used in the draw list item */
+	background_image_url_mobile: string;
+	/** Show if the draw is grand and is marked as special */
+	is_grand: boolean;
 }
