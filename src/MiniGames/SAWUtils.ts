e79c9ddb10c50d33423a823cc7b34f08bae6bb56
Oleh Yahont <oleh.y@smartico.ai>
2025-05-17 13:42:37 +0300

    SawUtils: add gems & diamonds support

--- src/MiniGames/SAWUtils.ts
+++ src/MiniGames/SAWUtils.ts
@@ -2,7 +2,7 @@ import { SAWBuyInType } from './SAWBuyInType';
 import { SAWTemplate } from './SAWTemplate';
 
 class SAWUtils {
-	public static canPlay = (t: SAWTemplate, balance: number): boolean => {
+	public static canPlay = (t: SAWTemplate, user: {ach_points_balance: number, ach_gems_balance: number, ach_diamonds_balance: number}): boolean => {
 		if (t === null || t === undefined) {
 			return false;
 		}
@@ -17,6 +17,12 @@ class SAWUtils {
 			case SAWBuyInType.Points:
 			case SAWBuyInType.Gems:
 			case SAWBuyInType.Diamonds: {
+				let balance = user.ach_points_balance;
+				if (t.saw_buyin_type_id === SAWBuyInType.Gems) {
+					balance = user.ach_gems_balance;
+				} else if (t.saw_buyin_type_id === SAWBuyInType.Diamonds) {
+					balance = user.ach_diamonds_balance;
+				}
 				return t.buyin_cost_points <= balance;
 			}
 			
