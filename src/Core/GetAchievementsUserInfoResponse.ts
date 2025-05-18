728fa4320b16647ac5da00752c1e33bfc4d57db5
Oleh Yahont <oleh.y@smartico.ai>
2025-05-18 08:26:09 +0300

    GetAchievementsUserInfoResponse: add gems and diamonds balance

--- src/Core/GetAchievementsUserInfoResponse.ts
+++ src/Core/GetAchievementsUserInfoResponse.ts
@@ -4,6 +4,8 @@ export interface GetAchievementsUserInfoResponse extends ProtocolResponse {
 	level_counter_1?: number;
 	level_counter_2?: number;
 	points_balance: number;
+    gems_balance: number;
+    diamonds_balance: number;
     points_ever: number;
     current_level: number;
     points_board_period_type_1: number;
