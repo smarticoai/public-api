d6e09dd1c3175f9617a8cff220adad9427850e35
Plamen Stamenov <plamen.stamenov.89@gmail.com>
2025-12-12 15:39:06 +0200

    BadgesTimeLimitStates fromating

--- src/Missions/BadgesTimeLimitStates.ts
+++ src/Missions/BadgesTimeLimitStates.ts
@@ -1,15 +1,15 @@
 export enum BadgesTimeLimitStates {
-	//Before Start Date → Show badge in greyed-out state with a "Locked by date" status.
+	/** Before Start Date */
 	BeforeStartDate = 0,
-	//After Start Date but before earned → Show badge in greyed-out state, without the "Locked by date" status.
+	/** After Start Date but before earned */
 	AfterStartDateNoProgress = 1,
-	//After Start Date but before started (no progress) + End date → Show badge in greyed-out state, without the "Locked by date" status, but with date stamp chip. The date is the End date.
+	/** After Start Date but before started (no progress) + End date */
 	AfterStartDateNoProgressAndEndDate = 2,
-	//After Start Date, but with some progress + End date → Show badge in colorful state, with date stamp chip. The date is the End date.
+	/** After Start Date, but with some progress + End date */
 	AfterStartDateWithProgressAndEndDate = 3,
-	//After End Date (not started) → Show badge in greyed-out state with a "Missed" status.
+	/** After End Date (not started) */
 	AfterEndDateNotStarted = 4,
-	//After End Date (player has some progress, but not completed) → Show badge colorful with a "Missed" status.
+	/** After End Date (player has some progress, but not completed) */
 	AfterEndDateWithProgress = 5,
 }
 
