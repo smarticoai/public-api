24f2f33e513587907a90dcd0c9bc1979594e3eba
Eugene Genov <eugene.g@smartico.ai>
2026-05-13 12:08:55 +0300

    [FIX] SMR-48074 - fixed error codes for joining clan

--- src/Clans/JoinClanErrorCode.ts
+++ src/Clans/JoinClanErrorCode.ts
@@ -5,18 +5,20 @@
 export enum JoinClanErrorCode {
 	/** No error, join completed */
 	JOIN_CLAN_OK = 0,
+	/** Invalid parameters */
+	JOIN_CLAN_INVALID_PARAMETERS = 1000,
 	/** clan_id doesn't exist for this label */
 	JOIN_CLAN_NOT_FOUND = 1001,
 	/** members_count >= capacity_limit */
 	JOIN_CLAN_FULL = 1002,
-	/** User is within the CLAN_COOLDOWN_DAYS window */
-	JOIN_CLAN_COOLDOWN_ACTIVE = 1003,
+	/** Not enough Points / Gems / Diamonds for entry fee */
+	JOIN_CLAN_INSUFFICIENT_FUNDS = 1003,
 	/** User doesn't satisfy entry_conditions / entry_segment_id */
 	JOIN_CLAN_SEGMENT_MISMATCH = 1004,
-	/** Not enough Points / Gems / Diamonds for entry fee */
-	JOIN_CLAN_INSUFFICIENT_FUNDS = 1005,
-	/** User is already a member of this clan */
-	JOIN_CLAN_ALREADY_MEMBER = 1006,
+	/** User is not in a clan */
+	JOIN_CLAN_USER_IS_NOT_IN_CLAN = 1005,
+	/** User is within the CLAN_COOLDOWN_DAYS window */
+	JOIN_CLAN_COOLDOWN_ACTIVE = 1006,
 	/** clan_status_id != 1 (clan is archived) */
 	JOIN_CLAN_ARCHIVED = 1007,
 	/** Clan joined after tournament start */
