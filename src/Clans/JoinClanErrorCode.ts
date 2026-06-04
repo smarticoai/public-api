9b7a40e0ab81588975f8a69aee2dd3557135a03f
Eugene Genov <eugene.g@smartico.ai>
2026-06-04 10:47:52 +0300

    [FIX] cleaning up the JoinClanErrorCode enum in public-api

--- src/Clans/JoinClanErrorCode.ts
+++ src/Clans/JoinClanErrorCode.ts
@@ -15,12 +15,16 @@ export enum JoinClanErrorCode {
 	JOIN_CLAN_INSUFFICIENT_FUNDS = 1003,
 	/** User doesn't satisfy entry_conditions / entry_segment_id */
 	JOIN_CLAN_SEGMENT_MISMATCH = 1004,
-	/** User is not in a clan */
+	/**
+	 * User is not in a clan.
+	 * Note: this is a `registerInTournament` response code that shares this enum.
+	 */
 	JOIN_CLAN_USER_IS_NOT_IN_CLAN = 1005,
 	/** User is within the CLAN_COOLDOWN_DAYS window */
 	JOIN_CLAN_COOLDOWN_ACTIVE = 1006,
-	/** clan_status_id != 1 (clan is archived) */
-	JOIN_CLAN_ARCHIVED = 1007,
-	/** Clan joined after tournament start */
+	/**
+	 * Clan joined after tournament start.
+	 * Note: this is a `registerInTournament` response code that shares this enum.
+	 */
 	JOIN_CLAN_JOINED_AFTER_TOURNAMENT_START = 1011,
 }
